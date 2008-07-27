107e28d53047ed2cb37d52240893c312efac3ecb
pvborza <pvborza@795c66b1-f152-0410-a59d-9bf392a7b71e>
2008-07-27 09:48:06 +0000

    modified some scripts for gesd

--- gesd/scripts/demoneo-echo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Runs gesd
-
-export LD_LIBRARY_PATH=../lib:$LD_LIBRARY_PATH
-exec ./gesd --neo --load ../config/echo.ges
---
+++ gesd/scripts/gesd-neo3.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Runs gesd on Neo FreeRunner for bottom accelerometer
+
+gesd --neo3 --config /etc/accelges/neo3/neo3.cfg
+
